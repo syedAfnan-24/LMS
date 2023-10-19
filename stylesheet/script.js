bc1c19c904c6cb7f08421494ab9e65512f6c86e3
syedAfnan-24 <syedliyaquathafnan@gmail.com>
2023-10-19 14:15:40 +0530

    added typeescript for navbar toggle button

--- stylesheet/script.js
+++ stylesheet/script.js
@@ -1,6 +1,6 @@
+"use strict";
 const mobileMenuButton = document.getElementById("mobile-menu");
 const menu = document.querySelector(".menu");
-
 mobileMenuButton.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
